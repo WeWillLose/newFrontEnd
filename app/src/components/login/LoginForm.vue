55c1d90820e3481a744b7f00556304d74cc53e9f
WeWillLose <artem2001sarg@mail.ru>
2021-04-17 01:41:52 +0300

    login form template

---
+++ app/src/components/login/LoginForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="std_form_container">
+    <q-form class="q-gutter-md q-ma-md" @submit="onSubmit">
+      <div><H4><strong>Авторизация</strong></H4></div>
+      <q-input type="text" label="Логин"/>
+      <q-input type="password" label="Пароль"/>
+      <div class="row justify-end ">
+        <q-btn label="Войти" :loading="loading" type="submit" icon="login" unelevated flat class="login_btn_tmp"></q-btn>
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<script lang="ts">
+  import {defineComponent, ref} from '@vue/composition-api';
+
+  export default defineComponent({
+    name: 'LoginPage',
+    components: {},
+    setup() {
+      const loading = ref(false)
+      return {loading};
+    },
+    methods: {
+      onSubmit():void{
+        this.loading = true;
+        setTimeout(() => {
+          this.loading = false;
+          this.$router.push("/")
+        },300)
+      }
+    }
+  });
+</script>
+
+<style scoped>
+
+</style>
--- app/src/pages/Login.vue
+++ app/src/pages/Login.vue
@@ -1,31 +1,15 @@
 <template>
   <q-page class="row items-center justify-center">
-    <div class="std_form_container">
-      <q-form class="q-gutter-md q-ma-md ">
-        <div><H4><strong>Авторизация</strong></H4></div>
-        <q-input type="text" label="Логин"/>
-        <q-input type="password" label="Пароль"/>
-        <div class="= row justify-end ">
-          <q-btn push label="Войти" @click="login" class="login_btn"></q-btn>
-        </div>
-
-      </q-form>
-    </div>
+    <login-form/>
   </q-page>
 </template>
 <script lang="ts">
   import {defineComponent, ref} from '@vue/composition-api';
-
+  import LoginForm from 'src/components/login/LoginForm.vue';
   export default defineComponent({
     name: 'LoginPage',
-    components: {},
+    components:{LoginForm},
     setup() {
-      return {};
-    },
-    methods: {
-      login():void{
-        this.$router.push("/")
-      }
     }
   });
 </script>
