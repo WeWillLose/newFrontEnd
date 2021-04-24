3148e7d9527fef9ef5395b8c57950a337e90158c
WeWillLose <artem2001sarg@mail.ru>
2021-04-24 20:21:06 +0300

    report step state to store

--- app/src/components/report/report_stepper/ReportStepper.vue
+++ app/src/components/report/report_stepper/ReportStepper.vue
@@ -464,137 +464,6 @@
   const Reconstruction = createNamespacedHelpers('report/reconstruction')
   const SDOHelper = createNamespacedHelpers('report/sdo')
   const MetaHelper = createNamespacedHelpers('report/meta')
-
-  const tablesStepperState = {
-    meta: {
-      error: false,
-      done:false
-    },
-    comment: {
-      error: false,
-      done:false
-    },
-    progress_exam: {
-      error: false,
-      done:false
-    },
-    progress:{
-      error:false,
-      done:false
-    },
-    creation:{
-      error:false,
-      done:false
-    },
-    proective:{
-      error:false,
-      done:false
-    },
-    class_rooms:{
-      error:false,
-      done:false
-    },
-    complex:{
-      error:false,
-      done:false
-    },
-    education:{
-      error:false,
-      done:false
-    },
-    programs:{
-      error:false,
-      done:false
-    },
-    reconstruction:{
-      error:false,
-      done:false
-    },
-    sdo:{
-      error:false,
-      done:false
-    },
-    teachingaids: {
-      error:false,
-      done:false
-    },
-    working_program: {
-      error:false,
-      done:false
-    },
-    circle: {
-      error:false,
-      done:false
-    },
-    events: {
-      error:false,
-      done:false
-    },
-    institutions: {
-      error:false,
-      done:false
-    },
-    plan: {
-      error:false,
-      done:false
-    },
-    activity: {
-      error:false,
-      done:false
-    },
-    coolhours: {
-      error:false,
-      done:false
-    },
-    obz: {
-      error:false,
-      done:false
-    },
-    plan_group: {
-      error:false,
-      done:false
-    },
-    content: {
-      error:false,
-      done:false
-    },
-    participation: {
-      error:false,
-      done:false
-    },
-    qualification: {
-      error:false,
-      done:false
-    },
-    selfeducation: {
-      error:false,
-      done:false
-    },
-    seminars: {
-      error:false,
-      done:false
-    },
-    technologies: {
-      error:false,
-      done:false
-    },
-    experience: {
-      error:false,
-      done:false
-    },
-    interaction: {
-      error:false,
-      done:false
-    },
-    subject: {
-      error:false,
-      done:false
-    },
-    manual: {
-      error:false,
-      done:false
-    },
-  }
   const lastInd = 32;
   export default defineComponent({
     name: 'ReportStepper',
@@ -628,7 +497,8 @@
       const reportData = ReportTableHelper.useGetters(['getData']).getData as unknown as Record<string, unknown>;
       const metaData = MetaHelper.useGetters(['getData']).getData as unknown as Record<string, unknown>;
       const store=() => context.root.$store;
-      console.log(context.root.$store.getters['report/comment/getScore'])
+      const tablesStepperState:Record<string,Record<string,boolean>> = ReportTableHelper.useState(['tablesState']).tablesState as unknown as Record<string,Record<string,boolean>>
+      console.log(tablesStepperState)
       return {step,stepper,meta,comment_table,tablesStepperState,lastInd,reportData,metaData,store}
     },
     methods: {
@@ -646,7 +516,7 @@
         result['data']['computed']['sum1'] = this.store().getters['report/comment/getScore']
           +this.store().getters['report/creation/getScore']+
           this.store().getters['report/proective/getScore']
-        this.store().commit('report/meta/setSum1',result['data']['computed']['sum1'])
+        this.store().commit('report/meta/setSum1', result['data']['computed']['sum1'])
         result['data']['computed']['sum2'] = 0
         return result;
       },
